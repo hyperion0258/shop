2daee4279c200f69160656de6ca8c2cd686e44ff
희윤 김 <hyperion0258@gmail.com>
2023-02-16 12:46:21 +0900

    styled-components 쓰면 CSS 파일 없어도 되는데

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,15 +1,27 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+// let YellowBtn = styled.button`
+//     background : yellow;
+//     color : black;
+//     padding : 10px;
+// `
+
+// let Box = styled.div`
+//     background : grey;
+//     padding : 20px;
+// `
 
 function Detail(props) {
 
-    let {id} = useParams();
-    let findGoods = props.shoes.find((data) => {return data.id == id})
+    let { id } = useParams();
+    let findGoods = props.shoes.find((data) => { return data.id == id })
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <img src={"https://codingapple1.github.io/shop/shoes" + ((findGoods.id)+1) + ".jpg"} width="100%" />
+                    <img src={"https://codingapple1.github.io/shop/shoes" + ((findGoods.id) + 1) + ".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
                     <h4 className="pt-5">{findGoods.title}</h4>
