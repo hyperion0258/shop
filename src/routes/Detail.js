e551b0fb01948ab4be8aeec5769db60c778f2db9
희윤 김 <hyperion0258@gmail.com>
2023-02-17 12:08:48 +0900

    Lifecycle과 useEffect 1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
 // let YellowBtn = styled.button`
@@ -12,13 +11,28 @@ import styled from 'styled-components';
 //     padding : 20px;
 // `
 
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+
+
 function Detail(props) {
+
+    useEffect(() => {
+        console.log('안녕')
+    })
+
+    let [count, setCount] = useState(0)
 
     let { id } = useParams();
     let findGoods = props.shoes.find((data) => { return data.id == id })
 
     return (
         <div className="container">
+            <div className='alert alert-warning'>
+                2초이내 구매시 할인
+            </div>
+            {count}
+            <button onClick={() => { setCount(count + 1) }}>버튼</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes" + ((findGoods.id) + 1) + ".jpg"} width="100%" />
